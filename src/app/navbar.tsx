981e033725b883164f895295b41175d78533f432
bingsu <bingyuan007@gmail.com>
2024-12-09 23:23:10 -0500

    front end configurations

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,40 +1,25 @@
-"use client";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
-import { client } from "./client";
 
-export default function Home() {
-  return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <CustomNavbar />
-      </div>
-    </main>
-  );
-}
-
-function CustomNavbar() {
+export default function navbar() {
     return (
         <Navbar position="static">
           <NavbarBrand>
-            <p className="font-bold text-inherit">namecard.</p>
+            <p className="font-bold text-inherit">ACME</p>
           </NavbarBrand>
           <NavbarContent className="hidden sm:flex gap-4" justify="center">
             <NavbarItem>
               <Link color="foreground" href="#">
-                marketplace
+                Features
               </Link>
             </NavbarItem>
             <NavbarItem isActive>
               <Link aria-current="page" href="#">
-                profile
+                Customers
               </Link>
             </NavbarItem>
             <NavbarItem>
               <Link color="foreground" href="#">
-                my wallet
+                Integrations
               </Link>
             </NavbarItem>
           </NavbarContent>
